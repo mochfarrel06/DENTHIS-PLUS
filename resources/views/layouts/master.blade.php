c32ba57db195d73001816427bd2e2136ad3cef9e
fremasadi <fremasadii@gmail.com>
2025-05-20 14:18:17 +0700

    replace dialog

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -142,7 +142,7 @@
                     showCancelButton: true,
                     confirmButtonColor: "#3085d6",
                     cancelButtonColor: "#d33",
-                    confirmButtonText: 'Ya, batalkan antrean!',
+                    confirmButtonText: 'Ya!',
                     cancelButtonText: 'Batal'
                 }).then((result) => {
                     if (result.isConfirmed) {
