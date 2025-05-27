d62a0a781fc9cf015466236625823e8399d90e19
MochFarrel06 <mochfarrel2606@gmail.com>
2025-05-27 21:27:46 +0700

    Modifikasi

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\OtpEmail;
 use App\Models\User;
+use App\Models\Verification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class VerificationController extends Controller
 {
@@ -15,9 +18,20 @@ class VerificationController extends Controller
     public function send_otp(Request $request) {
         if($request->type == 'register') {
             $user = User::find($request->user()->id);
-            dd($user);
+        } else {
+
         }
 
-        dd($request->all());
+        if (!$user) return back()->with('error', 'User Not found');
+        $otp = rand(100000, 999999);
+        $verify = Verification::create([
+            'user_id' => $user->id, 'unique_id' => uniqid(), 'otp' => md5($otp),
+            'type' => $request->type, 'send_via' => 'email'
+        ]);
+        Mail::to($user->email)->queue(new OtpEmail($otp));
+
+        if ($request->type == 'register') {
+            return redirect('/verify/'.$verify->unique_id);
+        }
     }
 }
